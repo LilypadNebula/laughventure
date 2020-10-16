41b8440fc540bde1bb4552e24069537dcc083a78
Lily Brenner <lilyneedsanemail@gmail.com>
2020-10-15 21:11:45 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col items-center">
+    <p class="text-3xl">Laughventure</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Laughventure',
+
+}
+</script>
