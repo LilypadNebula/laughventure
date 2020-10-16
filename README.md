55cac2320b687fd08b40189653d31b5d9fd3c0aa
Lily Brenner <lilyneedsanemail@gmail.com>
2020-10-15 22:15:26 -0400

    Display students, fetch users, randomize initial vals

--- README.md
+++ README.md
@@ -9,3 +9,13 @@ A project using Vue to create a simple laughing competition game
    yarn
    yarn dev
   ```
+
+## Requirements
+1. Using Vue (or another front end framework if you prefer), make a small website with some styling of your choice.
+2. Show 1 student on the left, and 1 student on the right. Display their name, HP and DPS as input elements.
+3. In the middle, display a "Compete" button to run the competition.
+4. In the middle, display the winner of the competition after pressing the "Compete" button.
+5. When the page loads, fetch the data from /users on https://jsonplaceholder.typicode.com
+6. Use a random name from the API for each student
+7. Use random HP and DPS for each student
+
--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,62 @@
 <template>
   <div class="flex flex-col items-center">
     <p class="text-3xl">Laughventure</p>
+    <div class="flex justify-around w-full">
+      <div class="flex flex-col items-center">
+        <p>{{firstStudent.name}}</p>
+        <p>HP: {{firstStudent.hp}}</p>
+        <p>DPS: {{firstStudent.dps}}</p>
+      </div>
+      <div>Winner</div>
+      <div class="flex flex-col items-center">
+        <p>{{secondStudent.name}}</p>
+        <p>HP: {{secondStudent.hp}}</p>
+        <p>DPS: {{secondStudent.dps}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
+<style>
+body{
+  @apply bg-blue-200;
+}
+</style>
+
 <script>
+import {onMounted, ref, reactive} from 'vue'
 
 export default {
   name: 'Laughventure',
+  setup(){
+    const nameList = ref([])
+    const firstStudent = reactive({name: '', hp: 0, dps: 0})
+    const secondStudent = reactive({name: '', hp: 0, dps: 0})
 
+    const randomNum = (max, min = 0) => {
+      return Math.floor(Math.random() * Math.floor(max)) + min
+    }
+
+    const randomName = () => {
+      let num = randomNum(nameList.value.length)
+      return nameList.value[num]
+    }
+
+    const randomizeStudent = (student) => {
+      student.name = randomName()
+      student.hp = randomNum(50, 10)
+      student.dps = randomNum(8,3)
+    }
+
+    onMounted(async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/users')
+      const users = await res.json()
+      nameList.value = users.map(user => user.name)
+      randomizeStudent(firstStudent)
+      randomizeStudent(secondStudent)
+    })
+
+    return {firstStudent, secondStudent}
+  }
 }
 </script>
